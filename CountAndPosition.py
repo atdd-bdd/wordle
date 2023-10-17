61640c9659712fe5d0f7fbb2e19e4a63a6677f12
Ken <ken.pugh@pugh-killeen.com>
2023-10-17 15:33:19 -0400

    minor formatting

--- CountAndPosition.py
+++ CountAndPosition.py
@@ -7,27 +7,22 @@ class CountAndPosition:
         t1 = Timer()
         t1.start()
         self.positions = {}
-        for c in "ABCDEFGHIJKLMNOPQRSTURVWXYZ":
+        for c in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
             self.positions[c] = [0, 0, 0, 0, 0]
 
         for word in words:
-            i = 0
-            for c in word:
+            for i, c in enumerate(word):
                 self.positions[c][i] += 1
-                i += 1
-        # print(self.positions)
+
         self.totals = {}
         for position in enumerate(self.positions.keys()):
             char = position[1]
             counts = self.positions[char]
-            # print("char ", char, "  count ", counts)
             total = 0
-
             for i in range(5):
                 total += counts[i]
             self.totals[char] = total
         Trace.write("Setting up counts " + t1.stop())
-        # print("totals ", self.totals)
 
     def zero_in_totals(self, char_seq):
         # print("Deleting from total ", char_seq)
